196737342bb57e2b16816a79f36bb49cf2d0a4c0
iluha <matvee05matveev@mail.ru>
2022-02-23 22:21:57 +0300

    first commit

--- sketche.js
+++ sketche.js
@@ -21,19 +21,6 @@ function draw() {
     background(150, 30, 220);
     
     fill(255);
-    push();
-    translate(x,y);
-    rotate(a);
-    rect(-20, -20, w, w);
-    pop()
-
-    a = a + 0.1;
-    y = y + 5;
-
-    if (y > height){
-        y = -50;
-        x = random(width);
-    }
 
     let targetX = mouseX;
     let dx = targetX - x1;
@@ -45,9 +32,5 @@ function draw() {
 
     fill(0);
     ellipse(x1, y1, w1, w1);
-    if (x+w>x1 && x<(x1+w1) && y+w>y1 &&        y<(y1+w1)) { 
-          y=random(100)*-1;
-          x=random(width);
-        }
 
 }
